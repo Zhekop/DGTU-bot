8e9b6f29d5a8dae156091a355f5219d2d198a1dc
«Im-Abyss» <enty3811@gmail.com>
2024-11-28 15:11:27 +0300

    начал писать функцию mywish

--- app/santa/wishes.py
+++ app/santa/wishes.py
@@ -1,9 +1,13 @@
 
 from .handler import RouterSanta
-
+from aiogram.types import CallbackQuery
 
 async def recipient()
 
-async def mywish()
+async def mywish(call: CallbackQuery):
+    await call.answer()
+    await call.message.answer('Напишите ваши пожелания')
+
+
 
 async def recipientwish()
