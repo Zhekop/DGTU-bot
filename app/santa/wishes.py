a801bb48ad8923d96a0451e862420bb57238ed47
«Im-Abyss» <enty3811@gmail.com>
2024-11-28 15:33:26 +0300

    FSM_santa

--- app/santa/wishes.py
+++ app/santa/wishes.py
@@ -3,6 +3,7 @@ from aiogram.fsm.context import FSMContext
 
 from .handler import RouterSanta
 from aiogram.types import CallbackQuery
+from utils.FSM import FSM_get
 
 async def recipient(call:CallbackQuery):
     await call.answer()
@@ -11,18 +12,20 @@ async def recipient(call:CallbackQuery):
         await call.message.answer(text='Пиши в лс дуралей')
         return
 
-async def mywish(call: CallbackQuery):
+async def mywish(call: CallbackQuery, state: FSMContext):
     await call.answer()
     await call.message.answer('Напишите ваши пожелания')
 
+    await state.set_state(FSM_get.GET_TEXT)
 
 
 async def recipientwish()
 
 
 
-async def FSM_santa(state:FSMContext):
-    state
+async def FSM_santa(message: Message, state: FSMContext):
+    await message.answer('Вы уверены в вашем пожелании?')
+    
 
 
 async def check(chat_id, user_id):
